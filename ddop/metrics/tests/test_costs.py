cb3de59af43c7d86b81dcc6420d5768534259e32
AndreasPhilippi <andreas.philippi1@stud-mail.uni-wuerzburg.de>
2021-05-03 14:19:12 +0200

    test tests

--- ddop/metrics/tests/test_costs.py
+++ ddop/metrics/tests/test_costs.py
@@ -10,7 +10,7 @@ class TestCosts(unittest.TestCase):
         self.assertEqual(pairwise_costs([10], [1], 2, 1), 18)
         assert_array_equal(pairwise_costs([1, 10], [2, 5], 1, 1), [[1], [5]])
         assert_array_equal(pairwise_costs([1, 10], [2, 5], 2, 1), [[1], [10]])
-        assert_array_equal(pairwise_costs([1, 10], [2, 5], 1, 2), [[2], [5]])
+        assert_array_equal(pairwise_costs([1, 10], [2, 5], 1, 2), [[2], [6]])
 
 
 if __name__ == '__main__':
