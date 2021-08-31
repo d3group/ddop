bfa1fe36d382e238235c674361a8ad2db66bc261
AndreasPhilippi <andreas.philippi1@stud-mail.uni-wuerzburg.de>
2021-08-31 13:44:03 +0200

    New version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='ddop',
-    version='v0.7.2',
+    version='v0.7.3',
     url='https://andreasphilippi.github.io/ddop/',
     license='MIT',
     author='Andreas Philippi',
