9c0a127dd1407ca6ed46c51f424cb8ec8dc3afad
AndreasPhilippi <andreas.philippi1@stud-mail.uni-wuerzburg.de>
2021-09-01 10:34:18 +0200

    ExponentialSmootingNewsvendor

--- ddop/newsvendor/_ExponentialSmoothingNewsvendor.py
+++ ddop/newsvendor/_ExponentialSmoothingNewsvendor.py
@@ -22,35 +22,66 @@ class ExponentialSmoothingNewsvendor(BaseNewsvendor, ClassicMixin):
        for each target variable
     trend : {"add", "mul", "additive", "multiplicative", None}, default=None
         Type of trend component.
-    damped : bool, optional (default=None)
+    damped_trend : bool, optional (default=None)
         Should the trend component be damped.
     seasonal : {"add", "mul", "additive", "multiplicative", None}, default=None
         Type of seasonal component.
     seasonal_periods : int, default=None
         The number of periods in a complete seasonal cycle, e.g., 4 for
         quarterly data or 7 for daily data with a weekly cycle.
+    initialization_method : str, optional
+        Method for initialize the recursions. One of:
+        * 'estimated'
+        * 'heuristic'
+        * 'legacy-heuristic'
+        * 'known'
+        If 'known' initialization is used, then `initial_level`
+        must be passed, as well as `initial_trend` and `initial_seasonal` if
+        applicable. Default is 'estimated'.
+    initial_level : float, optional
+        The initial level component. Required if estimation method is "known".
+        If set using either "estimated" or "heuristic" this value is used.
+        This allows one or more of the initial values to be set while
+        deferring to the heuristic for others or estimating the unset
+        parameters.
+    initial_trend : float, optional
+        The initial trend component. Required if estimation method is "known".
+        If set using either "estimated" or "heuristic" this value is used.
+        This allows one or more of the initial values to be set while
+        deferring to the heuristic for others or estimating the unset
+        parameters.
+    initial_seasonal : array_like, optional
+        The initial seasonal component. An array of length `seasonal`
+        or length `seasonal - 1` (in which case the last initial value
+        is computed to make the average effect zero). Only used if
+        initialization is 'known'. Required if estimation method is "known".
+        If set using either "estimated" or "heuristic" this value is used.
+        This allows one or more of the initial values to be set while
+        deferring to the heuristic for others or estimating the unset
+        parameters.
+    use_boxcox : {True, False, 'log', float}, optional
+        Should the Box-Cox transform be applied to the data first? If 'log'
+        then apply the log. If float then use lambda equal to float.
     smoothing_level : float, optional
         The alpha value of the simple exponential smoothing, if the value
         is set then this value will be used as the value.
-    smoothing_slope : float, optional
+    smoothing_trend : float, optional
         The beta value of the Holt's trend method, if the value is
         set then this value will be used as the value.
     smoothing_seasonal : float, optional
         The gamma value of the holt winters seasonal method, if the value
         is set then this value will be used as the value.
-    damping_slope : float, optional
-        The phi value of the damped method, if the value is
-        set then this value will be used as the value.
     optimized : bool, optional
         Estimate model parameters by maximizing the log-likelihood
-    use_boxcox : {True, False, 'log', float}, optional
-        Should the Box-Cox transform be applied to the data first? If 'log'
-        then apply the log. If float then use lambda equal to float.
     remove_bias : bool, optional
         Remove bias from forecast values and fitted values by enforcing
         that the average residual is equal to zero.
-    use_basinhopping : bool, optional
-        Using Basin Hopping optimizer to find optimal values
+    method : str, default "L-BFGS-B"
+        The minimizer used. Valid options are "L-BFGS-B" (default), "TNC",
+        "SLSQP", "Powell", "trust-constr", "basinhopping" (also "bh") and
+        "least_squares" (also "ls"). basinhopping tries multiple starting
+        values in an attempt to find a global minimizer in non-convex
+        problems, and so is slower than the others.
 
     Attributes
     ----------
@@ -87,32 +118,38 @@ class ExponentialSmoothingNewsvendor(BaseNewsvendor, ClassicMixin):
             cu=None,
             co=None,
             trend=None,
-            damped=False,
+            damped_trend=False,
             seasonal=None,
             seasonal_periods=None,
-            smoothing_level=None,
-            smoothing_slope=None,
-            smoothing_seasonal=None,
-            damping_slope=None,
-            optimized=True,
+            initialization_method="estimated",
+            initial_level=None,
+            initial_trend=None,
+            initial_seasonal=None,
             use_boxcox=False,
+            smoothing_level=None,
+            smoothing_trend=None,
+            smoothing_seasonal=None,
+            optimized=True,
             remove_bias=False,
-            use_basinhopping=False
+            method="L-BFGS-B"
     ):
         self.cu = cu
         self.co = co
         self.trend = trend
-        self.damped = damped
+        self.damped_trend = damped_trend
         self.seasonal = seasonal
         self.seasonal_periods = seasonal_periods
-        self.smoothing_level = smoothing_level
-        self.optimized = optimized
-        self.smoothing_slope = smoothing_slope
-        self.smoothing_seasonal = smoothing_seasonal
-        self.damping_slope = damping_slope
+        self.initialization_method = initialization_method
+        self.initial_level = initial_level
+        self.initial_trend = initial_trend
+        self.initial_seasonal = initial_seasonal
         self.use_boxcox = use_boxcox
+        self.smoothing_level = smoothing_level
+        self.smoothing_trend = smoothing_trend
+        self.smoothing_seasonal = smoothing_seasonal
+        self.optimized = optimized
         self.remove_bias = remove_bias
-        self.use_basinhopping = use_basinhopping
+        self.method = method
         super().__init__(
             cu=cu,
             co=co)
@@ -145,36 +182,25 @@ class ExponentialSmoothingNewsvendor(BaseNewsvendor, ClassicMixin):
         # Check and format under- and overage costs
         self.cu_, self.co_ = check_cu_co(self.cu, self.co, self.n_outputs_)
 
-        # Formate hyperparameter
-        self.trend = formate_hyperparameter(self.trend, "trend", self.n_outputs_)
-        self.damped = formate_hyperparameter(self.damped, "damped", self.n_outputs_)
-        self.seasonal = formate_hyperparameter(self.seasonal, "seasonal", self.n_outputs_)
-        self.seasonal_periods = formate_hyperparameter(self.seasonal_periods, "seasonal_periods", self.n_outputs_)
-        self.smoothing_level = formate_hyperparameter(self.smoothing_level, "smoothing_level", self.n_outputs_)
-        self.optimized = formate_hyperparameter(self.optimized, "optimized", self.n_outputs_)
-        self.smoothing_slope = formate_hyperparameter(self.smoothing_slope, "smoothing_slope", self.n_outputs_)
-        self.smoothing_seasonal = formate_hyperparameter(self.smoothing_seasonal, "smoothing_seasonal", self.n_outputs_)
-        self.damping_slope = formate_hyperparameter(self.damping_slope, "damping_slope", self.n_outputs_)
-        self.use_boxcox = formate_hyperparameter(self.use_boxcox, "use_boxcox", self.n_outputs_)
-        self.remove_bias = formate_hyperparameter(self.remove_bias, "remove_bias", self.n_outputs_)
-        self.use_basinhopping = formate_hyperparameter(self.use_basinhopping, "use_basinhopping", self.n_outputs_)
-
         forecasters = [ExponentialSmoothing(
             endog=y[:, i],
-            trend=self.trend[i],
-            damped=self.damped[i],
-            seasonal=self.seasonal[i],
-            seasonal_periods=self.seasonal_periods[i]) for i in range(self.n_outputs_)]
+            trend=self.trend,
+            damped_trend=self.damped_trend,
+            seasonal=self.seasonal,
+            seasonal_periods=self.seasonal_periods,
+            initialization_method=self.initialization_method,
+            initial_level=self.initial_level,
+            initial_trend=self.initial_trend,
+            initial_seasonal=self.initial_seasonal,
+            use_boxcox=self.use_boxcox) for i in range(self.n_outputs_)]
 
         fitted_forecasters = [forecasters[i].fit(
-            smoothing_level=self.smoothing_level[i],
-            optimized=self.optimized[i],
-            smoothing_slope=self.smoothing_slope[i],
-            smoothing_seasonal=self.smoothing_seasonal[i],
-            damping_slope=self.damping_slope[i],
-            use_boxcox=self.use_boxcox[i],
-            remove_bias=self.remove_bias[i],
-            use_basinhopping=self.use_basinhopping[i]) for i in range(self.n_outputs_)]
+            smoothing_level=self.smoothing_level,
+            smoothing_trend=self.smoothing_trend,
+            smoothing_seasonal=self.smoothing_seasonal,
+            optimized=self.optimized,
+            remove_bias=self.remove_bias,
+            method=self.method) for i in range(self.n_outputs_)]
 
         error = np.array([y[:, i] - fitted_forecasters[i].fittedvalues for i in range(self.n_outputs_)])
         error_mean = error.mean(axis=1)
