4ab00d8320f625e3edc191be159e2aa389ed18cf
AndreasPhilippi <andreas.philippi@uni-wuerzburg.de>
2022-04-02 13:39:30 +0200

    Fixed Sphinx error in Tutorial

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -50,9 +50,7 @@ templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-source_suffix = [
-    '.rst', '.ipynb'
-]
+source_suffix = ['.rst']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
